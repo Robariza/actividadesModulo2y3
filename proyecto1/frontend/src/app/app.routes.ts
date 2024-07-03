7ffee923fb87cf8aba253f2557aabf1e585f9797
Robariza <robinsonariza2001@gmail.com>
2024-07-02 19:33:56 -0500

    [UPDATE] admin

--- proyecto1/frontend/src/app/app.routes.ts
+++ proyecto1/frontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { InicioComponent } from './pages/inicio/inicio.component';
 import { IngresoComponent } from './pages/ingreso/ingreso.component';
 import { ProductosComponent } from './pages/productos/productos.component';
+import { AdminComponent } from './pages/admin/admin.component';
 
 export const routes: Routes = [
     {path:'inicio', component:InicioComponent},
     {path:'productos', component:ProductosComponent},
-    {path:'ingreso',component:IngresoComponent}
+    {path:'ingreso',component:IngresoComponent},
+    {path:'admin',component:AdminComponent}
 ];
