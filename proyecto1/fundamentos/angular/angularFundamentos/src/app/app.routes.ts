57e8b6f6ee96994d4ec371a5c776d4a24b33299b
Robariza <robinsonariza2001@gmail.com>
2024-07-17 16:05:27 -0500

    [update] routerlink, bootstrap

--- proyecto1/fundamentos/angular/angularFundamentos/src/app/app.routes.ts
+++ proyecto1/fundamentos/angular/angularFundamentos/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './pages/inicio/inicio.component';
 import { ProductosComponent } from './pages/productos/productos.component';
+import { NoEncontradoComponent } from './pages/no-encontrado/no-encontrado.component';
 
 // 1. importar las paginas del aplicativo
 // 2. definir la ruta de nuestras paginas
@@ -8,5 +9,6 @@ import { ProductosComponent } from './pages/productos/productos.component';
 export const routes: Routes = [
     {path: 'inicio', component: InicioComponent},
     {path: 'productos', component: ProductosComponent},
-    // {path: '', redirectTo: '/inicio', pathMatch: 'full'}
+    {path: '', redirectTo: '/inicio', pathMatch: 'full'},
+    {path: '**', component : NoEncontradoComponent} // ** -> cualquier otra ruta no definida 
 ];
--- proyecto1/fundamentos/angular/angularFundamentos/src/app/components/navegacion/navegacion.component.ts
+++ proyecto1/fundamentos/angular/angularFundamentos/src/app/components/navegacion/navegacion.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+// RouterLink permite la funcionalidad de enlace a las rutas por medio de routerlink
 import { RouterLink } from '@angular/router';
 
 @Component({
   // Esto se usa en html para poder usar el componente
   selector: 'app-navegacion',
   standalone: true,
+  // Importamos el componente a usar
   imports: [RouterLink],
   templateUrl: './navegacion.component.html',
   styleUrl: './navegacion.component.css'
